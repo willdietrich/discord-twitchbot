4456d61fdd8e0d16fa8e33a3f692e1e959cfb5a6
Will Dietrich <will.a.dietrich@gmail.com>
2017-11-08 00:34:25 -0600

    Delete dist directory.

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "./TwitchBot"], function (require, exports, TwitchBot_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const bot = new TwitchBot_1.TwitchBot();
-    bot.start();
-});
