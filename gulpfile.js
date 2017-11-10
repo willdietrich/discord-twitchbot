f0bbbad173d7f6b27c7bbd945f32babf1c86e043
Will Dietrich <will.a.dietrich@gmail.com>
2017-11-09 18:19:31 -0600

    update gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
+var del = require('del');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 
-var tsProject = ts.createProject('tsconfig.json');
-
 gulp.task('default', function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+    return gulp.src(['src/**/*.ts'], { base: 'src' })
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            module: "CommonJS",
+            noImplicitAny: true,
+            target: "ES6"
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return del([
+        'dist/**/*'
+    ]);
 });
